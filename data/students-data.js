649cf20aa321ecb067892c14f313de231fd07a45
ilysim <illymarie1@live.com>
2022-06-13 22:09:46 -0500

    add more info

--- data/students-data.js
+++ data/students-data.js
@@ -1,8 +1,8 @@
 const students = [
-  {name: 'Barbie', teen: true, _id: 23535},
-  {name: 'Ms. Rose', teen: false, _id: 290349},
-  {name: 'Bob', teen: false, _id: 2453},
-  {name: 'Ken', teen: true, _id: 2904854},
+  {name: 'Barbie', teen: true, food: 'apple', _id: 23535},
+  {name: 'Ms. Rose', teen: false, food: 'pizza', _id: 290349},
+  {name: 'Bob', teen: false, food: 'burger', _id: 2453},
+  {name: 'Ken', teen: true, food: 'soup', _id: 2904854},
 ]
 export {
   students
