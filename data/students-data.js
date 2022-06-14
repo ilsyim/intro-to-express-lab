2898611b4e1bf0f0e9976a18ca776e9b87192ea7
ilysim <illymarie1@live.com>
2022-06-13 21:26:03 -0500

    add students data

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Barbie', student: true, _id: 23535},
+  {name: 'Ms. Rose', student: false, _id: 290349},
+  {name: 'Bob', student: false, _id: 2453},
+  {name: 'Ken', student: true, _id: 2904854}
+]
+export {
+  students
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import modules
 
 import express from 'express'
-
+import { students } from './data/students-data.js'
 // Create Express app
 
 const app = express()
@@ -26,6 +26,12 @@ app.get('/home', function(req, res){
 
 app.get('/home', function(req, res){
   res.render('home')
+})
+
+app.get('/', function(req, res){
+  res.render('students/index', {
+    students: students
+  })
 })
 // Tell the app to listen on port 3000
 
